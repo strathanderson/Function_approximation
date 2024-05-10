3bafe9702f49201ce8758874f13cef53415b9682
Sam Anderson <sam.anderson@strath.ac.uk>
2024-05-10 11:55:20 +0100

    Creating repo

---
+++ test_cond.m
@@ -0,0 +1,25 @@
+clear all, close all, clc
+
+n = 32;
+b = 5*rand(n,1);
+w = n*rand(n,1);
+%w = ones(n,1);
+%b = linspace(0,1,n);
+L = pi;
+
+for i=1:n
+    for j=1:n
+        if w(i) ~= w(j)
+            M(i,j) = 1/2/(w(i)-w(j))*sin(L*w(i)-L*w(j)+b(i)-b(j))-...
+                1/2/(w(i)+w(j))*sin(L*w(i)+L*w(j)+b(i)+b(j))-...
+                1/2/(w(i)-w(j))*sin(b(i)-b(j))+...
+                1/2/(w(i)+w(j))*sin(b(i)+b(j));
+        else
+            M(i,j) = (2*L*cos(b(i)-b(j))*w(i)-sin(2*L*w(i)+b(i)+b(j)) +...
+                sin(b(i)+b(j)))/(4*w(i));
+        end
+    end
+end
+
+eig(M)
+cond(M)
